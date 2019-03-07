7bacc0e3f66cf19126c53340d919a4f8c4d7af43
thechromosomes <thesagar.sk@gmail.com>
2019-03-07 15:00:26 +0530

    neon_moon

---
+++ app.js
@@ -0,0 +1,41 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+
+const apiKey = '90a9239076898a3eb4b312fe42227774';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null, arg: null, sun: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.area;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+   request(url, function (err, response, body) {
+    if(err){
+      console.log(`you just typed that does not exist... or check the internet connection`);
+      res.render('index', {weather: null, arg: null, error: 'Error, please try again... you enter something that does not exist!'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, arg: null, error: 'Error, please try again... you enter something that does not exist!'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees celsius in ${weather.name}!`;
+        let arg = `pressure is ${weather.main.pressure} hPa, humidity is ${weather.main.humidity}%`;
+        console.log(`this is full report`);
+        console.log(weather);
+        res.render('index', {weather: weatherText, error: null, arg: arg});
+      }
+    }
+  });
+})
+
+app.listen(5000, function () {
+  console.log('app listening on port 5000!')
+})
